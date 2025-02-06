3e76cf9cdba4aff3eef555c5b40ad973868d5cfc
Nisrayani13 <nisrayani.13@gmail.com>
2025-02-06 21:49:47 +0530

    made nav bar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,36 +1,111 @@
-"use client"
+"use client";
 
-import { Search, User, ShoppingCart } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
+import { Search, User, ShoppingCart, X } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { Menu } from "lucide-react";
 
 export default function Navbar() {
-  const [isMounted, setIsMounted]=useState(false)
+  const [isMounted, setIsMounted] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  useEffect(()=>{
-    setIsMounted(true)
-  },[])
-  
-  if(!isMounted) return <div>Loading....</div>
+  const toggleMenu = () => {
+    setIsMenuOpen((isMenuOpen) => !isMenuOpen);
+  };
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return <div>Loading....</div>;
 
   return (
-    <div className='fixed top-0 left-0 z-50 w-full flex flex-wrap md:flex-nowrap justify-between items-center px-8 py-3 border-b shadow-sm text-sm md:text-base'>
-      
-      <div className='flex justify-between gap-3'>
-        <Search></Search>
-        <input type="text" placeholder='Search'/>
+    <nav className="fixed top-0 left-0 z-10 w-full flex justify-between items-center px-8 py-3 border-b shadow-sm text-sm md:text-base">
+      {/* Left */}
+      <div className="flex justify-between items-center gap-6">
+        <div className="font-bold text-4xl">JSX</div>
+        <div className="hidden md:flex">
+          <SearchCompos isMobile={false}></SearchCompos>
+        </div>
       </div>
-      <div className='flex justify-between gap-x-20'>
-        <a>Home & Living</a>
-        <a>Accessories</a>
-        <a>Stationery</a>
+
+      {/* Center */}
+      <div className="hidden md:flex justify-between gap-x-20">
+        <Navlinks isMobile={false}></Navlinks>
       </div>
-      <div className='flex justify-between items-center gap-4'>
-        <button className='border border-gray-300 rounded-full p-2 items-center justify-center'>
-        <ShoppingCart ></ShoppingCart>
+
+      {/* Right */}
+      <div className="flex justify-between items-center gap-6">
+        <button className="border border-gray-300 rounded-full p-2 items-center justify-center">
+          <ShoppingCart></ShoppingCart>
         </button>
-        <User ></User>
-        <div>Log In</div>
+        <div className="hidden md:flex">
+          <LoginCompos isMobile={false}></LoginCompos>
+        </div>
+
+        {/* Smaller screens menu bar */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu}>
+            {isMenuOpen ? null : <Menu></Menu>}
+          </button>
+        </div>
+        {isMenuOpen && (
+          <div className="h-full w-full fixed top-0 left-0 right-0 z-50 bg-white p-3">
+            <MenuOptions toggleMenu={toggleMenu}></MenuOptions>
+          </div>
+        )}
       </div>
-    </div>
-  )
+    </nav>
+  );
 }
+
+const Navlinks = ({isMobile}:any) => {
+  return (
+    <>
+      <Link href="/" className={`${isMobile?"text-xl":"text-base"}`}>Home & Living</Link>
+      <Link href="/accesories" className={`${isMobile?"text-xl":"text-base"}`}>Accesories</Link>
+      <Link href="/stationery" className={`${isMobile?"text-xl":"text-base"}`}>Stationery</Link>
+    </>
+  );
+};
+
+const LoginCompos = ({isMobile}:any) => {
+  return (
+    <div className="flex justify-center items-center gap-3">
+      <User></User>
+      <div className={`${isMobile?"text-xl":"text-base"}`}>Log In</div>
+    </div>
+  );
+};
+
+const SearchCompos = ({isMobile}:any) => {
+  return (
+    <div className="flex justify-between items-center gap-3">
+      <Search></Search>
+      <input type="text" placeholder="Search" className={`${isMobile?"text-xl":"text-base"}`}/>
+    </div>
+  );
+};
+
+const MenuOptions = ({ toggleMenu }: any) => {
+  return (
+    <div className="flex justify-between items-start md:hidden">
+      <div className="flex flex-col gap-3 items-start">
+        <div className="flex flex-col items-start gap-3 border-b-2 border-footerBackground p-3">
+          <div>
+            <LoginCompos isMobile={true}></LoginCompos>
+          </div>
+          <div>
+            <SearchCompos isMobile={true}></SearchCompos>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 items-start p-3">
+          <Navlinks isMobile={true}></Navlinks>
+        </div>
+      </div>
+      <button onClick={toggleMenu} className="p-3">
+        <X></X>
+      </button>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col font-serif">
       <div className="min-w-full">
         <Navbar></Navbar>
       </div>
-      <div className="flex-1 pt-30 md:pt-16">Hiiiiiiiiiiiii</div>
-      <Footer></Footer>
+      {/* <div className="flex-1 pt-30 md:pt-16">Main body</div>
+      <Footer></Footer> */}
     </div>
   );
 }
